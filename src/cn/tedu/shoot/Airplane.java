75966a860f759b4ee2412a00931a674141e3f4c6
123ZZH <xxxxxxxxxx@qq.com>
2022-07-11 11:24:59 +0800

    first commit

--- src/cn/tedu/shoot/Airplane.java
+++ src/cn/tedu/shoot/Airplane.java
@@ -11,6 +11,7 @@ public class Airplane extends FlyingObject implements Enemy {
 		height = image.getHeight(); //高
 		Random rand = new Random(); //随机数对象
         System.out.println("*****");
+        System.out.println("11121212121");
 		x = rand.nextInt(ShootGame.WIDTH-this.width); //x:0到(窗口宽-敌机宽)之内的随机数
 		y = -this.height; //y:负的敌机的高
 	}
